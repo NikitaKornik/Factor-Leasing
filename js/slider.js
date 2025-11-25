8c3dc27a96ff21fe7a426a5cfa012f0a49588661
NikitaKornik <nikita0927@mail.ru>
2025-11-25 20:02:26 +0200

    slider fix

--- js/slider.js
+++ js/slider.js
@@ -83,25 +83,3 @@ skipBtn.addEventListener("click", nextSlide);
 
 // --- Инициализация ---
 startSlide(current);
-
-// --- АНИМАЦИЯ fade-ap ---
-
-document.addEventListener("DOMContentLoaded", () => {
-  const elements = document.querySelectorAll("slider__fade");
-
-  const observer = new IntersectionObserver(
-    (entries, obs) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          entry.target.classList.add("show");
-          obs.unobserve(entry.target); // больше не наблюдаем
-        }
-      });
-    },
-    {
-      threshold: 0.1, // срабатывает, когда 10% элемента видно
-    }
-  );
-
-  elements.forEach((el) => observer.observe(el));
-});
